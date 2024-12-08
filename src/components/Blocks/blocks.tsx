f5657d237839f577dfcccc5d40b66231c433892b
vyakulin <vyakulindaniel@mail.ru>
2024-12-08 20:57:08 +0300

    links and input-line fix

--- src/components/Blocks/blocks.tsx
+++ src/components/Blocks/blocks.tsx
@@ -1,6 +1,6 @@
 'use client';
 import '@/components/Blocks/style.scss';
-import { Lnk, Props } from '../Atoms/atoms';
+import { Button, Lnk, Props } from '../Atoms/atoms';
 import clsx from 'clsx';
 import { FaExplosion } from 'react-icons/fa6';
 import { usePathname } from 'next/navigation';
@@ -21,7 +21,7 @@ const createLink = (name : string) => {
     href: `/${name.toLowerCase()}`
   }
 }
-const links = [createLink('Documentation'), createLink('Contact')]
+const links = [createLink('Documentation'), createLink('Contact'), createLink('Donate')]
 
 export function Header() {
   const path = usePathname();
@@ -34,10 +34,14 @@ export function Header() {
       <div className={clsx({
         [`header-links`]: true,
       })}>
-        {links.map((item, index) => {
+        {links.map((item, index, array) => {
           return (
-            <Lnk color={item.href === path ? 'a' : 'b'} style='-underline' key={index} href={item.href}>
+            index + 1 !== array.length ? <Lnk color={item.href === path ? 'a' : 'b'} style='-underline' key={index} href={item.href}>
               {item.name}
+            </Lnk> : <Lnk key={index} href={item.href}>
+              <Button color={item.href === path ? 'a' : 'b'} style='ol'>
+                {item.name}
+              </Button>
             </Lnk>
           )
         })}
